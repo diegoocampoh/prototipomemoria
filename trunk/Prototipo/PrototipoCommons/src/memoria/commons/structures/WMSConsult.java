1311695d84f1fbbca6c25039e5964e72f1833194
buitronfranklin@msn.com <buitronfranklin@msn.com@cf036833-93a3-e103-9140-00bf6c4ed1e1>
2011-10-03 22:23:16 +0000

    Agregue el objeto WMSConsult es el objeto que uso para pasar a xml la consulta recibida por el usuario

---
+++ trunk/Prototipo/PrototipoCommons/src/memoria/commons/structures/WMSConsult.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package memoria.commons.structures;
+
+import memoria.commons.structures.coordinates.Coordinate;
+
+/**
+ *
+ * @author Fran
+ */
+public class WMSConsult {
+    private Coordinate SW;
+    private Coordinate NE;
+    private Coordinate Centro;
+    private String[] Capas;
+
+    public Coordinate getSW()
+    {
+        return this.SW;
+    }
+    public Coordinate getNE()
+    {
+        return this.NE;
+    }
+
+    public Coordinate getCentro()
+    {
+        return this.Centro;
+    }
+
+    public String[] getCapas()
+    {
+        return this.Capas;
+    }
+    public void setSW(Coordinate unSW)
+    {
+        this.SW = unSW;
+    }
+    public void setNE(Coordinate unNE)
+    {
+        this.NE = unNE;
+    }
+    public void setCentro(Coordinate unCentro)
+    {
+        this.Centro = unCentro;
+    }
+    public void setSW(String[] unaCapas)
+    {
+        this.Capas = unaCapas;
+    }
+
+
+
+
+
+}
