298cdb886bf85d7caeaed012f354fa2d62ccec11
buitronfranklin@msn.com <buitronfranklin@msn.com@cf036833-93a3-e103-9140-00bf6c4ed1e1>
2011-10-23 16:54:54 +0000

    C

--- trunk/Prototipo/Servlet/src/java/ManejadorRespuesta.java
+++ trunk/Prototipo/Servlet/src/java/ManejadorRespuesta.java
@@ -183,7 +183,7 @@ public class ManejadorRespuesta {
     {
         //C:\Program Files\SlikSvn\bin
         //String fileName = "C:\\Users\\Fran\\Desktop\\Validaciones\\Test01.kml C:\\Users\\Fran\\prototipomemoria\\Prototipo\\KMLs\\KMLFB.kml";
-        String fileName = "/var/www/html/kml/Resultado.kml";//"C:\\Users\\Fran\\prototipomemoria\\Prototipo\\KMLs\\Fuego.kml";//
+        String fileName = /*"/var/www/html/kml/Resultado.kml";//*/"C:\\Users\\Fran\\prototipomemoria\\Prototipo\\KMLs\\Fuego2.kml";//
         FileWriter fw;
         BufferedWriter bw;
         String archivo = "<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n <kml xmlns=\"http://www.opengis.net/kml/2.2\"> \n <Document>\n";
